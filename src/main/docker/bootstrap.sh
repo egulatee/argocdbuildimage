fe28a9ed856d502a2ab19e89d8fbc8ec664657d0
Eric Gulatee <eric.gulatee@gmail.com>
2021-12-03 23:07:50 -0500

    argo

--- src/main/docker/bootstrap.sh
+++ src/main/docker/bootstrap.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-set -eux
+echo "Param0=" $0
+echo "Param1=" $1
+echo "Param2=" $2
+echo "Param3=" $3
+echo "Param4=" $4
+
+#set -eux
 #​git clone ssh://<git-repo-domain>/<reponame>
 #cd <reponame>
-echo "Param=" $1
-set +u
-if [ -z "$GIT_HASH" ] # Check if GIT_HASH exists
-then
-    set -u
-    echo "GIT_HASH is empty, skipping git reset"
-else
-    set -u
-    git reset --hard $GIT_HASH
-fi
+#echo "Param=" $1
+#set +u
+#if [ -z "$GIT_HASH" ] # Check if GIT_HASH exists
+#then
+#    set -u
+#    echo "GIT_HASH is empty, skipping git reset"
+#else
+#    set -u
+#    git reset --hard $GIT_HASH
+#fi
 ​
 #echo Running $1
 #source $1
