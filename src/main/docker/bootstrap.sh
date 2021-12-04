152db9aaa0c86dbe32658e0bae647063e6d3fb9e
Eric Gulatee <eric.gulatee@gmail.com>
2021-12-04 15:51:25 -0500

    ...

--- src/main/docker/bootstrap.sh
+++ src/main/docker/bootstrap.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#base64 --help
-
 echo 'Saving message'
 echo $1 > message.base64
 echo 'Saved message'
@@ -13,19 +11,3 @@ echo 'Decoded message'
 
 echo < base64.json
 
-#set -eux
-#​git clone ssh://<git-repo-domain>/<reponame>
-#cd <reponame>
-#echo "Param=" $1
-#set +u
-#if [ -z "$GIT_HASH" ] # Check if GIT_HASH exists
-#then
-#    set -u
-#    echo "GIT_HASH is empty, skipping git reset"
-#else
-#    set -u
-#    git reset --hard $GIT_HASH
-#fi
-​
-#echo Running $1
-#source $1
